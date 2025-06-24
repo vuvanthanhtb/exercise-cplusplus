4ea9d3ce4818026aed85799cc36af54f9cdd31bf
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-25 00:57:03 +0700

    day 2

--- exercise_11.cpp
+++ exercise_11.cpp
@@ -4,5 +4,40 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, d;
+        cin >> n >> d;
+        int a[n];
+
+        int q = 0;
+        while(q < n) {
+            cin >> a[q];
+            q++;
+        }
+
+        int arr[n];
+        int f = 0;
+        int g = 0;
+        int h = d;
+        while(f < n) {
+            if(h < n) {
+                arr[f] = a[h];
+                h++;
+            } else {
+                arr[f] = a[g];
+                g++;
+            }
+            f++;
+        }
+
+        int l = 0;
+        for(l = 0; l < n; l++) {
+            cout << arr[l] << " ";
+        }
+        cout << endl;
+
+    }
     return 0;
 }
--- exercise_12.cpp
+++ exercise_12.cpp
@@ -3,6 +3,75 @@
 
 using namespace std;
 
+bool isExist(int arr[], int n, int found) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == found) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, m;
+        cin >> n >> m;
+
+        int matrix[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int row[n];
+        int col[m];
+        for(int i = 0; i < m; i++) {
+            col[i] = -1;
+        }
+
+
+        for(int i = 0; i < n; i++) {
+            row[i] = -1;
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == 1) {
+                    row[i] = i;
+                    col[j] = j;
+                }
+            }
+        }
+
+        int result[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                result[i][j] = 0;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(isExist(row, n, i)) {
+                    result[i][j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(isExist(col, m, i)) {
+                    result[j][i] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                cout << result[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
     return 0;
 }
