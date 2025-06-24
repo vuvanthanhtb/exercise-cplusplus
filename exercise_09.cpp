99384cfc647a507d73f68d6711c2c27af5d44998
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-24 22:25:12 +0700

    day 1

--- exercise_09.cpp
+++ exercise_09.cpp
@@ -1,9 +1,36 @@
 // CHÊNH LỆCH NHỎ NHẤT
 #include <iostream>
+#include<math.h>
 
 using namespace std;
 
 int main() {
+    int t, n;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        int a[n];
+        int k = 0;
+        while(k < n) {
+            cin >> a[k];
+            k++;
+        }
+        int min = a[0];
+        int j = 0;
+        while(j < n) {
+            int f = j + 1;
+            while (f < n)
+            {
+                int sub = abs(a[j] - a[f]);
+                if(min > sub) {
+                    min = sub;
+                }
+                f++;
+            }
+            j++;
+        }
+        cout << min << endl;
+    }
 
     cout << endl;
     return 0;
--- exercise_10.cpp
+++ exercise_10.cpp
@@ -1,10 +1,64 @@
-// SỐ MAY MẮN
+// SỐ NHỎ NHẤT CHƯA XUẤT HIỆN
 #include <iostream>
 
 using namespace std;
 
-int main() {
+bool isPresent(int arr[], int n, int x) {
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == x) {
+      return true;
+    }
+  }
+  return false;
+}
 
-    cout << endl;
-    return 0;
+void sortArray(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (arr[i] > arr[j]) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+
+    int a[n];
+    int k = 0;
+    while (k < n) {
+      cin >> a[k];
+      k++;
+    }
+
+    sortArray(a, n);
+
+    int m = 0;
+    int currentSmallest = a[n - 1] + 1;
+    while (m < n) {
+      int current = a[m];
+      if (current > 0) {
+        int i = 1;
+        while (i < current) {
+          bool found = isPresent(a, m, i);
+          if (!found) {
+            currentSmallest = i;
+            break;
+          }
+          i++;
+        }
+      }
+      m++;
+    }
+
+    cout << currentSmallest << endl;
+  }
+  return 0;
 }
