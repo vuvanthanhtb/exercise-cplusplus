4f9f54ecf1d9af67daff2187ba97370f7e13ea31
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-25 01:19:36 +0700

    day 2

--- exercise_13.cpp
+++ exercise_13.cpp
@@ -4,5 +4,40 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+
+        int k = 0;
+        int a[n * n];
+        while(k < n * n) {
+            cin >> a[k];
+            k++;
+        }
+
+
+        int matrix[n][n];
+        int l = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                matrix[i][j] = a[l];
+                l++;
+            }
+
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    cout << matrix[i][j] << " ";
+                } else {
+                    cout << " "  << " ";
+                }
+            }
+            cout << endl;
+        }
+
+    }
     return 0;
 }
