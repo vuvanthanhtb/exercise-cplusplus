1177db7845a1f59ffac9254a0c310aa51c687cb3
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-25 13:02:21 +0700

    day 2

--- exercise_16.cpp
+++ exercise_16.cpp
@@ -1,8 +1,48 @@
 // NHỎ NHẤT - LỚN NHẤT
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+int sumDigits(int num, int n) {
+    int sum = 0;
+    int k = n;
+    while(k) {
+        sum += num % 10;
+        num = num / 10;
+        k--;
+    }
+
+    return sum;
+}
+
 int main() {
+    int m, s;
+    cin >> m >> s;
+
+    int limit = pow(10, m);
+
+    int k = pow(10, m - 1);
+    int min = limit;
+    int max = -1;
+    while(k < limit) {
+        if(sumDigits(k, m) == s) {
+            if(min > k) {
+                min = k;
+            }
+            if(max < k) {
+                max = k;
+            }
+        }
+
+        k++;
+    }
+
+    if(min == limit) {
+        min = -1;
+    }
+
+
+    cout << min << " " << max << endl;
     return 0;
 }
