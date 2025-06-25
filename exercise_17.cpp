c9cdfe54a44152d8e93cbef37cfa650122e4d686
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-25 14:13:58 +0700

    day 2

--- exercise_17.cpp
+++ exercise_17.cpp
@@ -1,8 +1,31 @@
 // ĐẦU CUỐI GIỐNG NHAU
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void numOfSameStartEnd() {
+    string s;
+    getline(cin, s);
+    int count = 0;
+    int len = s.length();
+    for(int i = 0; i < len; i++) {
+        for(int j = i; j < len; j++) {
+            if(s[i] == s[j]) {
+                count++;
+            }
+        }
+    }
+    cout << count << endl;
+}
+
 int main() {
+    int t;
+    cin >> t;
+    getchar();
+    while(t--) {
+        numOfSameStartEnd();
+    }
+
     return 0;
 }
