fcf49799c1017374dc108f453cdf415a29901998
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-25 14:44:49 +0700

    day 1

--- exercise_18.cpp
+++ exercise_18.cpp
@@ -1,8 +1,30 @@
 // LOẠI BỎ NGUYÊN ÂM
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string removeVowels(string input) {
+    string vowels = "ueoaiUEOAI";
+    string output = "";
+    int len = input.length();
+
+    for(int i = 0; i < len; i++) {
+        if (vowels.find(input[i]) == string::npos)
+        {
+            char c = tolower(input[i]);
+            output += ".";
+            output += c;
+        }
+    }
+
+    return output;
+}
+
 int main() {
+    string s;
+    getline(cin, s);
+
+    cout << removeVowels(s) << endl;
     return 0;
 }
