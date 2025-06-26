e012e53acd87c069e33dfb916128bad1341fd76c
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-26 08:34:54 +0700

    day 3

--- exercise_27.cpp
+++ exercise_27.cpp
@@ -4,5 +4,42 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+
+    int arr[n][n];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int m;
+    cin >> m;
+    int sub[m][m];
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> sub[i][j];
+        }
+    }
+
+    int r = 0;
+    int c = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            r = r == m ? 0 : r;
+            c = c == m ? 0 : c;
+            arr[i][j] *= sub[r][c];
+            c++;
+        }
+        r++;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
