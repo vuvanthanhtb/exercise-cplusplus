b8c1dce66d0e07fbbda2f51c58775ba5f6882e19
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-26 15:58:32 +0700

    day 3

--- exercise_34.cpp
+++ exercise_34.cpp
@@ -1,8 +1,42 @@
 // DANH SÁCH SINH VIÊN - 1
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
+struct Student {
+    string student_code;
+    string full_name;
+    string student_class;
+    string date_of_birth;
+    float gpa;
+
+    void display() {
+        cout << fixed << setprecision(2);
+        string date = date_of_birth.size() != 10 ? string("0" + date_of_birth) : date_of_birth;
+        cout << student_code << " " << full_name << " " << student_class << " " << date << " " << gpa << endl;
+    }
+};
+
+
 int main() {
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i <= n) {
+        Student s;
+        s.student_code = "B20DCCN0" + string(i < 10 ? "00" : "0") + to_string(i);
+        cin.ignore();
+        getline(cin, s.full_name);
+        getline(cin, s.student_class);
+        getline(cin, s.date_of_birth);
+        cin >> s.gpa;
+
+        s.display();
+        i++;
+    }
+
+
     return 0;
 }
