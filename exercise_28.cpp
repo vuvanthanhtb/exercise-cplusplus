b55d010ffa42279e7772a40ee85a5fb8ceda22cc
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-26 08:59:36 +0700

    day 3

--- exercise_28.cpp
+++ exercise_28.cpp
@@ -1,8 +1,42 @@
-// SỐ MAY MẮN
+// ĐỔI CHỖ CHỮ SỐ
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+
+        int arr[100];
+
+        int num = n;
+        int index = 0;
+        while(num) {
+            arr[index] = num % 10;
+            num /= 10;
+            index++;
+        }
+
+        for(int i = index - 2; i > 0; i--) {
+            if(arr[i] > arr[i - 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+                break;
+            }
+        }
+
+        int new_num = 0;
+        for(int i = 0; i < index; i++) {
+            new_num += arr[i] * pow(10, i);
+        }
+
+        cout << (new_num == n ? -1 : new_num) << endl;
+
+    }
     return 0;
 }
