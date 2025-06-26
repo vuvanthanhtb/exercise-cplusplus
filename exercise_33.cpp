eb65e1830a2e09d5ac73c8af8d9ad006cabd2b36
Vũ Văn Thanh <vuvanthanhtb@gmail.com>
2025-06-26 15:16:11 +0700

    day 3

--- exercise_33.cpp
+++ exercise_33.cpp
@@ -4,5 +4,45 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int a[n];
+
+        for(int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(a[i] < a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        int result[n];
+        int indexStart = 0;
+        int indexEnd = n - 1;
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                result[i] = a[indexStart];
+                indexStart++;
+            } else {
+                result[i] = a[indexEnd];
+                indexEnd--;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            cout << result[i] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
